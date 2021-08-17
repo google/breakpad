698f00668a0f6e0ceb41f91e9b60b84762b2aa62
Matt Cooper <matt.cooper@bugsnag.com>
2021-08-17 11:30:49 +0100

    Populate new fields on Event and MinidumpMetadata

--- src/processor/bugsnag_stackwalk_wrapper.cc
+++ src/processor/bugsnag_stackwalk_wrapper.cc
@@ -346,7 +346,8 @@ static Event getEvent(const ProcessState& process_state) {
                        .exception = e,
                        .app = app,
                        .device = device,
-                       .threads = getThreads(process_state)};
+                       .threads = getThreads(process_state),
+                       .unhandled = process_state.crashed()};
 
   return returnEvent;
 }
@@ -627,7 +628,7 @@ static uint32_t getModuleAnnotations(Minidump& dump,
   return moduleInfoItemCount;
 }
 
-MinidumpMetadata getMinidumpMetadata(Minidump& dump) {
+MinidumpMetadata getMinidumpMetadata(Minidump& dump, const ProcessState& process_state) {
   SimpleAnnotation* simpleAnnotationArray = nullptr;
   ModuleInfo* moduleInfoArray = nullptr;
 
@@ -657,7 +658,9 @@ MinidumpMetadata getMinidumpMetadata(Minidump& dump) {
       .moduleCount = static_cast<int>(moduleInfoCount),
       .moduleInfo = moduleInfoArray};
 
-  MinidumpMetadata minidumpMetadata = {.crashpadInfo = crashpadInfo};
+  MinidumpMetadata minidumpMetadata = {
+    .crashpadInfo = crashpadInfo,
+    .assertion = duplicate(process_state.assertion().c_str())};
 
   return minidumpMetadata;
 }
@@ -710,7 +713,7 @@ WrappedEvent GetEventFromMinidump(const char* filename,
     result.event = getEvent(process_state);
 
     // Populate metadata
-    result.event.metaData = getMinidumpMetadata(dump);
+    result.event.metaData = getMinidumpMetadata(dump, process_state);
 
   } catch (const std::exception& ex) {
     string errMsg = "encountered exception: " + string(ex.what());
